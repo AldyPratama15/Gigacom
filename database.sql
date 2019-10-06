8f0304fc79b8b9607c6df8b05ecb1dc937fcd2a7
pratamaldy09@gmail.com <pratamaaldy98>
2019-10-06 21:34:49 +0700

    database baru ada triger suplai

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2019 at 08:14 PM
+-- Generation Time: Oct 06, 2019 at 04:34 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,6 +23,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `inventori`
+--
+
+CREATE TABLE IF NOT EXISTS `inventori` (
+`id_barang` int(11) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `kategori` enum('A','B','C','D') NOT NULL,
+  `stok` tinyint(4) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventori`
+--
+
+INSERT INTO `inventori` (`id_barang`, `nama_barang`, `kategori`, `stok`, `harga`) VALUES
+(1, 'LAPTOP ASUS A442UR', 'A', 15, 7600000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventori_masuk`
+--
+
+CREATE TABLE IF NOT EXISTS `inventori_masuk` (
+`id` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventori_masuk`
+--
+
+INSERT INTO `inventori_masuk` (`id`, `id_barang`, `stok`, `tanggal`) VALUES
+(1, 1, 1, '2019-10-06'),
+(2, 1, 3, '2019-10-06');
+
+--
+-- Triggers `inventori_masuk`
+--
+DELIMITER //
+CREATE TRIGGER `tambah_stok` AFTER INSERT ON `inventori_masuk`
+ FOR EACH ROW BEGIN
+	UPDATE inventori SET stok= stok+NEW.stok
+    WHERE id_barang= NEW.id_barang;
+    END
+//
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pegawai`
 --
 
@@ -32,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
   `alamat` text NOT NULL,
   `telp` varchar(13) NOT NULL,
   `username` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pegawai`
@@ -68,6 +122,18 @@ INSERT INTO `user` (`username`, `password`, `jabatan`) VALUES
 --
 
 --
+-- Indexes for table `inventori`
+--
+ALTER TABLE `inventori`
+ ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `inventori_masuk`
+--
+ALTER TABLE `inventori_masuk`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
@@ -84,10 +150,20 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `inventori`
+--
+ALTER TABLE `inventori`
+MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `inventori_masuk`
+--
+ALTER TABLE `inventori_masuk`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
-MODIFY `id_pegawai` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id_pegawai` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
